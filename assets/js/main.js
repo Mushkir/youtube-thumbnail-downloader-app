104343506ddb084c966cbe29455ba9ed6ff15b60
Mohamed Mushkir <mushkirmohamed@gmail.com>
2024-01-09 04:36:30 +0530

    feat: validation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,72 @@
-import JustValidate from "just-validate"
-import isUrl from 'is-url'
+// JS Library for Form Validation
+import JustValidate from "just-validate";
 
+// JS Library for Validate YouTube URL
+import youtubeUrl from "youtube-url";
 
-const urlFormEl = document.querySelector("#urlForm")
+const urlFormEl = document.querySelector("#urlForm");
 const validate = new JustValidate(urlFormEl, {
-    validateBeforeSubmitting: true
-})
+  validateBeforeSubmitting: true,
+});
 
-
-validate.addField("#youtube-url-input", [
+validate.addField(
+  "#youtube-url-input",
+  [
     {
-        rule: 'required',
+      rule: "required",
     },
-
-], {
-    errorLabelCssClass: ['form-error']
-})
-
+  ],
+  {
+    errorLabelCssClass: ["form-error"],
+  }
+);
 
 validate.onSuccess((e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const formData = new FormData(urlFormEl)
+  // Get the youtube URL https://www.youtube.com/watch?v=ehhIzVYD-M8
+  const formData = new FormData(urlFormEl);
+  const youtubeURLObj = Object.fromEntries(formData.entries());
+  const UrlData = youtubeURLObj["youtube-url-input"];
+  // console.log(UrlData);
 
 
-    // Get the youtube URL https://www.youtube.com/watch?v=ehhIzVYD-M8
-    const youtubeURLObj = Object.fromEntries(formData.entries())
-    const UrlData = youtubeURLObj["youtube-url-input"]
-    // console.log(UrlData);
-
+  // Verify the URL is valid or not
+  if (youtubeUrl.valid(UrlData) == true) {
     // Get ID
-    const splittedURL = UrlData.split("v=")
-    
+    const splittedURL = UrlData.split("v=");
 
     // Need to add ID with https://i.ytimg.com/vi/[VIDEO_ID]/maxresdefault.jpg
-    const urlID = splittedURL[1].split("").slice(0,11)
-    const thumbnailURL = `https://i.ytimg.com/vi/${urlID.join("")}/maxresdefault.jpg`
+    const urlID = splittedURL[1].split("").slice(0, 11);
+    const thumbnailURL = `https://i.ytimg.com/vi/${urlID.join("")}/maxresdefault.jpg`;
 
     // console.log();
-    // console.log(thumbnailURL);
+    console.log(thumbnailURL);
 
     // Need to show Thumbnail image in UI
+  const thumbnailViewer = document.querySelector("#thumbnail-viewer")
 
-})
+  const thumbnailImageEl = document.createElement("img")
+  thumbnailImageEl.src = thumbnailURL
+  thumbnailImageEl.classList.add("w-[1000px]", "h-[1000px]", "object-cover", "mx-auto")
+  
 
 
+//   Download Button
+const downloadButtonEl = document.createElement("a");
+downloadButtonEl.href = thumbnailURL;
+downloadButtonEl.download = `Thumbnail Image ${0 + 1}`;
+downloadButtonEl.textContent = "Download Thumbnail";
+downloadButtonEl.classList.add("text-white"); 
+// thumbnailViewer.appendChild(downloadButtonEl);
+thumbnailViewer.appendChild(thumbnailImageEl)
+  
 
+  urlFormEl.reset()
 
+  } else {
 
+    alert ("Sorry! This field requires only YouTube URL.")
+  }
+
+});
